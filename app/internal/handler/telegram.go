99f031bd5325c1d3635febeb7ee791b6fce11fb9
nikolay.korchik <n_korchik@boxberry.ru>
2024-04-27 21:32:17 +0300

    логирование входящего запроса

--- app/internal/handler/telegram.go
+++ app/internal/handler/telegram.go
@@ -1,13 +1,17 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"net/http/httputil"
 	"pasha_bot"
 )
 
 func (h *Handler) TelegramWebhook(c *gin.Context) {
 	var req []pasha_bot.Input
+	forLog, _ := httputil.DumpRequest(c.Request, true)
+	fmt.Println(string(forLog))
 	if err := c.ShouldBindJSON(&req); err != nil {
 		return
 	}
